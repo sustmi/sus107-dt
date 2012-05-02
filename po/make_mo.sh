c79baadceb6c020e2112b41e981b9d107deefc6e
Miroslav Šustek <sustmidown@centrum.cz>
2012-05-02 03:06:38 +0200

    czech translation

--- po/make_mo.sh
+++ po/make_mo.sh
@@ -4,6 +4,6 @@ echo "* Generating cs.mo..."
 mkdir -p ../lang/cs/LC_MESSAGES
 msgfmt -c -v -o ../lang/cs/LC_MESSAGES/sus107-dt.mo cs.po
 
-echo "* Generating en.mo..."
-mkdir -p ../lang/en/LC_MESSAGES
-msgfmt -c -v -o ../lang/en/LC_MESSAGES/sus107-dt.mo en.po
+#echo "* Generating en.mo..."
+#mkdir -p ../lang/en/LC_MESSAGES
+#msgfmt -c -v -o ../lang/en/LC_MESSAGES/sus107-dt.mo en.po
